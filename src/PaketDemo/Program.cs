6a8fac964f0f53c8308d3cf956be10861ab492aa
MarkRGray <mark.gray@agent3.com>
2016-09-04 16:23:22 +0100

    Pushing all src in pre-paket state

---
+++ src/PaketDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PaketDemo
+{
+    public class Person
+    {
+        public string FirstName { get; set; }      
+        public string LastName { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fred = new Person {FirstName = "Fred", LastName = "Smith"};
+
+            var fredJson = JsonConvert.SerializeObject(fred);
+
+            Console.WriteLine(fredJson);
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
+        }
+    }
+}
